5b73571690e7ae8ac5e79c030185581448fa4f73
Andrew Smith <ansmit@microsoft.com>
2024-12-03 08:57:07 +0000

    Tidy day 3 solution

--- day3.py
+++ day3.py
@@ -1,31 +1,28 @@
 import helpers
 import re
 
-def run_part1(path):
-    search = r"mul\((\d+),(\d+)\)"
-    total = 0
-    for line in helpers.open_file(path):
-        results = re.findall(search, line)
-        for p in results:
-            total += int(p[0]) * int(p[1])
-    print(total)
-
-def run_part2(path):
-    search = r"mul\((\d+),(\d+)\)|(d)o\(\)|do(n)\'t\(\)"
+def run(path, search):
     total = 0
     active = True
     for line in helpers.open_file(path):
         results = re.findall(search, line)
         for p in results:
-            if (p[2] == "d"):
-                active = True
-                continue
-            if (p[3] == "n"):
-                active = False
-                continue
+            if (len(p) > 2):
+                if (p[2] == r'do'):
+                    active = True
+                    continue
+                if (p[2] == r"don't"):
+                    active = False
+                    continue
             if (active):
                 total += int(p[0]) * int(p[1])
-    print(total)
+    return total
+
+def run_part1(path):
+    print(run(path, r"mul\((\d+),(\d+)\)"))
+
+def run_part2(path):
+    print(run(path, r"mul\((\d+),(\d+)\)|(do(?:n't)?)\(\)"))
 
 run_part1("test_data/day3.txt")
 run_part1("data/day3.txt")
